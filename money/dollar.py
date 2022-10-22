81ae26e3710df2911ab3d97bb6e7855c7a126fc3
kohei kawazoe <a04272017@gmail.com>
2022-10-22 22:38:20 +0900

    Change initialization `amount`

--- money/dollar.py
+++ money/dollar.py
@@ -1,10 +1,10 @@
 
 class Dollar:
 
-    amount = 10
+    amount = 0
 
     def __init__(self, amount: int):
         pass
 
     def times(self, multiplier: int):
-        pass
+        self.amount = 5 * 2

c9e55b28ae896a94bf9039ed57ef8e01cc3ff970
kohei kawazoe <a04272017@gmail.com>
2022-10-22 23:15:57 +0900

    Change a test for new interface
    We want to prevent `amount` from changing in the `times` method.

--- tests/test_money.py
+++ tests/test_money.py
@@ -3,5 +3,8 @@ from money import dollar
 
 def  test_multiprication():
     five = dollar.Dollar(5)
-    five.times(2)
-    assert five.amount == 10
+    product = five.times(2)
+    assert product.amount == 10
+
+    product = five.times(3)
+    assert product.amount == 15

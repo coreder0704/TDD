0a71837b9c0638e789d5ac563f0a62674238cdd6
kohei kawazoe <a04272017@gmail.com>
2022-10-31 23:59:48 +0900

    Refactoring a test
    Python CANNOT compare equality between instaces in default.
    It need a instance method `__eq__()`.

--- money/dollar.py
+++ money/dollar.py
@@ -5,6 +5,9 @@ class Dollar:
     def __init__(self, amount: int):
         self.amount = amount
 
+    def __eq__(self, __o: object) -> bool:
+        return self.amount == __o.amount
+
     def times(self, multiplier: int):
         return Dollar(self.amount * multiplier)
 
--- tests/test_money.py
+++ tests/test_money.py
@@ -4,7 +4,7 @@ from money import dollar
 def  test_multiprication():
     five = dollar.Dollar(5)
     product = five.times(2)
-    assert product.amount == 10
+    assert product == dollar.Dollar(10)
 
     product = five.times(3)
     assert product.amount == 15

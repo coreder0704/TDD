0815cebf4416fde8cded5444df4a2d6388b0d872
kohei kawazoe <a04272017@gmail.com>
2022-11-03 18:42:46 +0900

    genelization of "_amount" and equals()

--- money/franc.py
+++ money/franc.py
@@ -1,16 +1,7 @@
 from __future__ import annotations
+from money import money
 
-class Franc():
-
-    def __init__(self, amount: int):
-        self._amount = amount
-
-    def __eq__(self, other: Franc) -> bool:
-        return self.__dict__ == other.__dict__ and \
-            self.__class__ == other.__class__
+class Franc(money.Money):
 
     def times(self, multiplier: int) -> Franc:
         return Franc(self._amount * multiplier)
-
-    def equals(self, other: Franc) -> bool:
-        return self._amount == other._amount
